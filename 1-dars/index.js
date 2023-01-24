264b0cdf2a90e64cdc571bec7f12ba6260f0d5b8
Samar0208 <ttymi1994@gmail.com>
2023-01-24 17:14:42 +0500

    1-dars

---
+++ 1-dars/index.js
@@ -0,0 +1,63 @@
+// let text;
+
+// console.log(text);
+// ==================================undefined
+
+// let text;
+// console.log(text);
+// text = " Webbrain Academy";
+// ==================================undefined
+
+// let text;
+// text = "Webbrain Academy";
+// console.log(text);
+// ================================="Webbrain Academy"
+
+// let num = 1,
+//   a = 2,
+//   b = 3;
+// console.log(num, a, b);
+
+// let a = (b = c = 3);
+// console.log(a, b, c);
+
+// ==================================="Data Types"=====================
+
+// console.log(12345678912345678n + 1n);
+// console.log('Welcome to "Webbrain"');
+// let name = "Webbrain Academy";
+
+// console.log("Welcome " + name + ", IT Center");
+// console.log(`Welcome ${name} IT Center`);
+
+// ======================================="Null vs Undefined"==============
+
+// let a;  undefined
+// let b = null; null
+// console.log(a, b);
+
+// console.log(null === undefined); false
+// console.log(null == undefined); ture
+
+// ======================================="Primitive NON-Primitive"==============
+
+// let a;
+// let b = null;
+// console.log(a + 1); null
+// console.log(b + 1); 1
+
+// let a;
+// let b;
+// console.log(b === b);  true
+// console.log({ b } === { b }); false
+
+// ======================================="Symbols"==============
+
+// let a = Symbol("text");  folse
+// let b = Symbol("text");  folse
+// console.log(b === a);
+// console.log(b == a);
+
+// ======================================="Primitive NON-Primitive"==============
+
+// Object -array, obj, function
---
+++ 1-dars/savollar.js
@@ -0,0 +1,13 @@
+// JS nima - js biror bir saytimizni xarakatlanishi uchun kerak bo'ladi
+// JS qanday ishlaydi-
+// O'zgarivchi ochilib unga malumot biriktrilmasa - Undefined qaytaradi
+// Text qachon is nod defined chiqadi- consol.log(text)deb yozadigan bolsak lekin text degan malumot yo'q shunda is nod defined chiqadi
+// Variables - let, var, const  let-kam xotradan joy oladi kerakli joyda ishlaydi qolgan vaqtda joyni bo'shatib quyadi
+// let bilam var ning farqi - let - scop, var - global;
+// Data Types - JS da 8ta  data type lar mavjud ulardan 7 tasi primitive, 1tasi non- primitive
+// Number, Bigint, String, Boolean, Null, Undefined, Symbols,
+// NON-primitive - Object, arry, function
+// Primitev  data taype value qaytaradi
+// Non-primitive refrins qaytaradi yani link
+// Symbols- yunik id bir biriga tengmas
+// Object-array, obj, function bularning data type object
