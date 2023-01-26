dae8b567792d82ee1ee28dbd49fdb3968c30a7ad
Samar0208 <ttymi1994@gmail.com>
2023-01-26 16:40:38 +0500

    test

---
+++ 2-dars/index.js
@@ -0,0 +1,69 @@
+// let a = 1;
+// let b = "1";
+// console.log(a + b);
+
+// let str = "Webbrain";
+// let str1 = "Academy";
+
+// console.log(str + str1); Webbrain Academy
+
+// let a = "1"; false
+// let a = "1s"; true
+
+// console.log(isNaN(a));
+
+// let a = 1;
+// let b = "1";
+// console.log(a + +b);
+
+// let a = "10";
+// let b = "10.3";
+
+// console.log(Number(a)); 10
+// console.log(Number.parseInt(a)); 10
+// console.log(Number.parseFloat(a)); 10
+
+// console.log(Number(b)); 10.3
+// console.log(Number.parseInt(b)); 10
+// console.log(Number.parseFloat(b)); 10.3
+
+// ========================== true=!0, !null, !undefined ==================
+
+// console.log(true == 1); // true
+// console.log(false == 0); // true
+// console.log(false == null); // false
+// console.log(false == undefined); // false
+// console.log(undefined == null); //true
+
+// console.log(true == "1"); // true
+// console.log(false == "a"); // false
+// console.log(true == "2"); // false
+
+// let a = 1;
+// let b = "10";
+// console.log(a / +b);
+// console.log(8 % 3);
+// console.log(111 % 10);
+
+// console.log(1 + (1 * 1) / 1); // 2
+// console.log(1 + (1 * 1) / 1); // 2
+
+// =================== incement--  decrement++  =============
+
+// let a = 11;
+// a++;
+// a = a + 1;
+// console.log(a++); // 11
+// console.log(a); // 12
+// console.log(++a); // 13
+// console.log(a); // 13
+// console.log(a++); // 13
+// console.log(a++); // 14
+// console.log(++a); // 15
+
+// ======================= Obrivations=========
+
+// let a = 50;
+// a = a + 10; // 60
+// a += 10; // 60+10=70
+// console.log(a);
--- 2-dars/savollar.js
+++ 2-dars/savollar.js
@@ -1,1 +1,21 @@
 // Single Thread nima - bir boshidsn ish boshlaydi yani tepadan pastga qarab bittadan ish bajaradi
+// JS da nechi xil compint qilsa bo'ladi-
+// HTML da script da src yozsa, yozmasa nima bo'ladi-
+// Virabil nima-
+// Var, Let, Const niam-
+// Nechta data taype mavjud-
+// Primitiv va non-primitiv-
+// Null qanday data taype- data taype object uzi primitiv
+// Null bn Undefined farqi-
+// Array, Function, Object bularning farqlari nima-
+// Array bn Function farqi nima-
+// siNaN nima - berilgan malumotni NaN yoki NaN emasligini tekshirib beradi
+// Number parseInt nima- parseInt yaxlitlab beradi
+//console.log(Number.parseInt(b)); 10
+// Number parseFloat nima- parseFloat numerni qanday xolatda bulsa shu xolatini chiqarib beradi
+// console.log(Number.parseFloat(b)); 10.3
+// Boolen nima- true false
+// true= 1, "1"
+// false=> 0
+// console.log(ture == 1); true,   console.log(false==null); false, console.log(undefined==null); false
+// console.log(false == 0) true, console.log(false==undefined); true
