e9c3c474dec0b271af39d8d006285c9b187cae65
Samar0208 <ttymi1994@gmail.com>
2023-02-06 15:54:22 +0500

    function

---
+++ 5-dars/homework.js
@@ -0,0 +1,81 @@
+// ================= 1-Homework ===========
+
+// for (let i = 0; i < 3; i++) {
+//   console.log(`number ${i}`);
+// }
+
+// let i = 0;
+// while (i < 3) {
+//   console.log(`number${i}`);
+//   i++;
+// }
+// bu kodimizda bitta o'zgarivchi yaratib uni nulga tengalab quyayamiz. While loop da ishalatamiz i dan katta 3 deb olamiz va har bir aylanganini consolga chiqaradi bunda  bizga i++ deb yozganmiz bu har bir aylanishni bittaga oshiradi;
+
+// =============== 2-Homework ======================
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// if (checkAge(19)) {
+//   console.log("dbfgu");
+// } else {
+//   console.log("false");
+// }
+
+// ================= 3 ===============
+
+// function min(a, b) {
+//   if (a > b) {
+//     return b;
+//   } else {
+//     return a;
+//   }
+// }
+
+// console.log(min(2, 5)); // 2
+// console.log(min(5, -5)); // -5
+// console.log(min(5, 5)); // 1
+
+// function pow(x, n) {
+//   s = 0;
+
+//   for (i = 0; i < n; i++) {
+//     s += x;
+//   }
+//   return s;
+// }
+
+// console.log(pow(5, 5));
+
+// function pow(x, n) {
+//   s = 1;
+//   for (i = 1; i <= n; i++) {
+//     s *= x;
+//   }
+//   return s;
+// }
+// console.log(pow(3, 2)); // ==9
+
+// function pow(x, n) {
+//   x = 1;
+//   for (i = 1; i <= n; i++) {
+//     x *= n;
+//   }
+//   return x;
+// }
+// console.log(pow(3, 3)); // 27
+
+// function pow(x, n) {
+//   x = 1;
+//   for (i = 1; i <= n; i++) {
+//     x *= n;
+//   }
+//   return x;
+// }
+
+// console.log(pow(1, 100)); // 1e+200
