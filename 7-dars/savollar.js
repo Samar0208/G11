f8cdcf5e38b75b92ed7205f6bb83d0c356983770
Samar0208 <ttymi1994@gmail.com>
2023-02-10 16:42:07 +0500

    Numbers

--- 7-dars/savollar.js
+++ 7-dars/savollar.js
@@ -1,11 +1,39 @@
+// savollar
 //=====  Number ===
 
 // toFixed()- bu uzining ichida bitta parametr qabul qiladi yani bnechta lementgacha aylansin, hamda yax litlab ham beradi, toFixed numberni aftamatik tarizda string qivoradi endi bini numberga alantrish ucun "+ yoki Number" deb yozib quyamzi;
+
 // Numberda eval() nima - eval() bu numberda qnadaya amal bo'lsa ham to'g'ri ishlaydi;
+
 // toString - Numberni stringa uzgartirib beradi;
 
-// floor() nima- bu har doim biga berilagan kasr sonlarni butun qismini olib beradi yani 1.6 bulsa 1 qaytaradi ParseIntga uxshaydi;
+// floor() nima- bu har doim bizga berilagan kasr sonlarni butun qismini olib beradi yani 1.6 bulsa 1 qaytaradi ParseIntga uxshaydi;
+
 // ceil() nima - bu har doim sonlarni yuqorisini oladi yani 1.6 bo'lsa 2 qaytaradi berilgan qiymatga 1 quchib quyadi;
-// round() numa - bu kasr sonlarni 1.2 bulsa bir qaytaradi 1.5 va undan katta sonlarga 1 qushib qaytaradi;
+
+// round() nima - bu kasr sonlarni 1.2 bulsa 1 qaytaradi 1.5 va undan katta sonlarga 1 qushib qaytaradi;
+
 // pow() nima - bu darjani chiqarib beradi misol uchun 2**4 ni pow(2,4) deb yozsak =16 chiqadi;
-// sign() nima - bu sonning +, -, 0 ekanligini ifodalaydi: misp uchun console.log(Math.sign(12)) // 1 agar shuning oldida  menus bo'lsa -1  chiqaradi nul bo'sa nulni chiqaradi;
+
+// sign() nima - ishora degan manoni bildradi bu sonning +, -, 0 ekanligini ifodalaydi: misol uchun console.log(Math.sign(12)) // 1 agar shuning oldida  menus bo'lsa -1  chiqaradi nul bo'sa nulni chiqaradi;
+
+// eval() nima - bu string ishidagi sonlarni chiqarib beradi namber taypega utqazadi agar brorta harf aralashsa error qaytaradi;
+
+// Object.is() - bu bizga true, false qaytaradi Misol uchun ("2","2") // true,( 2, 2) // true, ({}, {}) // false, (2, "2")// false;
+
+// Math.abs() - bunga manfiy son kirtsa ham musbat uzgartrib quyadi;
+
+// Math.trunc() - bu sonning butun qismini oladi katta ham qilmaydi kichchik ham qilamayd;
+
+// =======================================
+
+// 1. PI-3.14
+// 2. abc() - absalut value, only musbat;
+// 3. floor() - kasr qismini pastga qarab yaxlitlaydi;
+// 4. ceil() - kasr qismini yuqoriga qarab yaxlitlaydi;
+// 5. round() - yaxlitlaydi .5 => tepaga 5 d  an kichiki bo'lsa pastga;
+// 6. trunc() - kasir qismini tushrib qoldiradi;
+// 8. rondom() - 1~0 oroligidagi rondom raqam;
+// 9. min(numbers) - sonlar orasidan minimumni topib beradi;
+// 10. max(numbers) - sonlar orasidan maximumini topib beradi;
+// 11. pow(num.pow) - darsjaga kupaytradi;
