d4ea09b474fbeb6e00ba46f76ed6a4ec821aa59d
Samar0208 <ttymi1994@gmail.com>
2023-02-14 17:04:42 +0500

    String

---
+++ 8-dars/index.js
@@ -0,0 +1,19 @@
+// let str1 = "webbrain";
+// let str2 = new String("webbra");
+
+// console.log(str2 == "webbrain");
+// console.log(str2.localeCompare(str1));
+
+let str = "webbrain academy";
+// console.log(str1.indexOf("bb", 5)); // -1
+// console.log(str1.startsWith("web")); // true
+// console.log(str1.endsWith("emy")); // true
+// console.log(str.padEnd(10, "."));
+// console.log(str.padStart(18, "."));
+// console.log(str.slice(0, 2));
+// console.log(str.substring(0, 3));
+// console.log(eval("2+5")); // 7
+
+for (char of str) {
+  console.log(char);
+}
---
+++ 8-dars/savollar.js
@@ -0,0 +1,14 @@
+// localeCompare() -1, 0, 1  -  bunda -1 bunaqa qiymat umuman yo'q degani , 0 qisman bor, 1 esa egzektik bir biriga mos keladi;
+// indexOf("") - bunga qanaqa malumot kiritsak qaysi indexsda turganligini ko'rsatadi;
+// lastIndexOf("") - bu malumotni oxiridan boshlan qidiradi va qasi indexda ekanligini aniqlaydi;
+// includes("") - bu bizga tru false qaytaradi berilgan malumotni kiritsak bor yo'qligini aytadi;
+// startsWith("") - bu bizga malumat qaysi so'z bn boshlanganligi xaqida malumot qataradi; true, false;
+// endsWith("") - bu esa qaysi so'z bnlan tugaganligini qaytaradi; true, false;
+// padEnd("") - bu oxiriga nuqtalar  quyib chiqaradi malumot ko'ayin ketsa;
+// padStart("") - bu boshiga nuqta quyib chiqaradi
+// slice() - bu bizga malumotlarni kesib beradi;
+// subString() - bu ham malumot kesib beradi;
+// eval("") - eval string ichidagi arximetik amalni chiqarib beradi;
+// trim() - malumotni oldidagi yoki orqasidagi bo'sh joylarni uchirib yuboradi
+// split() - bu malumotni ajratib chiqarib beradi;
+// concat() - bu malumotga yana biror bir so'z qushib quyadi;
