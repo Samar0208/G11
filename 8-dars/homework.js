f22fe350e7af7362c4b868dfc12a60b74103b78c
Samar0208 <ttymi1994@gmail.com>
2023-02-15 17:20:40 +0500

    String

---
+++ 8-dars/homework.js
@@ -0,0 +1,10 @@
+// ============== HomeWork ===========
+
+//! 1
+
+let str = "webrain academy";
+let str1 = "webbrain academy";
+let str2 = "webbrain academy";
+
+console.log(str2.localeCompare(str)); // -1
+console.log(str2.localeCompare(str1)); // 1
