ae1252f29d5b78afb1d829f47aac215a37a70307
Samar0208 <ttymi1994@gmail.com>
2023-02-28 08:11:34 +0500

    date

---
+++ 12-dars/index.js
@@ -0,0 +1,11 @@
+let date=new Date()
+
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getTime());
+console.log(Date.now());
+
+let month=["Yanvar","Fevral"]
+console.log(month[date.getMonth()]);
