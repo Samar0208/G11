28ad3434d93bf4c4730c27a4a21ce19d57c4d35a
Samar0208 <ttymi1994@gmail.com>
2023-03-01 14:31:35 +0500

    date

--- 12-dars/homework.js
+++ 12-dars/homework.js
@@ -1,31 +1,6 @@
-// const getToday = (symbol) => {
-//   let date = new Date();
-//   return `Today: ${date.getDate()}${symbol}${
-//     date.getMonth() + 1
-//   }${symbol}${date.getFullYear()}`;
-// };
-// console.log(getToday("-"));
-// console.log(getToday("/"));
-
-// let weeks = ["Dushanba", "sesa"];
-// let date = new Date();
-// console.log(weeks[date.getDay()].slice(0, 4));
-// console.log(weeks[date.getDay()-1]);
-
-// const moment = (symbol) =>
-// {
-//     let weeks = ["yanvar", "fevral", "mart"]
-//     let a = ["mp"]
-//     let date = new Date()
-
-//     return `tody: ${weeks[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${a}`
-// }
-// console.log(moment());
-
-
-
 // ============ LT ==========
-
+let date = new Date();
+let months = ["yanvar", "fevral", "mart", "Aprel"];
 
 // const moment=(symbol)=>{
 //     let date= new Date()
@@ -34,7 +9,6 @@
 // return `hour: ${LT}`
 // }
 // console.log(moment(":")); // hour: 7:50
-
 
 // ============== LTS ======
 
@@ -47,7 +21,6 @@
 // console.log(moment(":")); // hours: 7:56:32
 
 // ============== L ==================
-
 
 // const moment=(nol, symbol)=>{
 //     let date= new Date()
@@ -69,13 +42,158 @@
 
 // ========== LL =========
 
-const moment = (symbol) =>
-{
-    let date = new Date()
-    let weeks = ["Yanvar", "Fevral", "Mart"]
+// const moment = (symbol) => {
+//   let date = new Date();
+//   let months = ["Yanvar", "Fevral", "Mart"];
 
-    let format = [`${weeks[date.getMonth()]} ${date.getDate()}${symbol} ${date.getFullYear()}`]
-    let LL = format
-    return `presentDay: ${LL}`
-}
-console.log(moment(",")); // presentDay: Mart 1, 2023
+//   let format = [
+//     `${
+//       months[date.getMonth()]
+//     } ${date.getDate()}${symbol} ${date.getFullYear()}`,
+//   ];
+//   let LL = format;
+//   return `presentDay: ${LL}`;
+// };
+// console.log(moment(",")); // presentDay: Mart 1, 2023
+
+//=============== ll ==============
+
+// const moment = (symbol) => {
+//   let date = new Date();
+//   let months = ["Yanvar", "Fevral", "Mart"];
+//   let format = [
+//     `${months[date.getMonth()].slice(
+//       0,
+//       3
+//     )} ${date.getDate()}${symbol} ${date.getFullYear()}`,
+//   ];
+//   let ll = format;
+//   return `presentDay: ${ll}`;
+// };
+
+// console.log(moment(",")); // presentDay: Mar 1, 2023
+
+// ============ LLL =========
+
+// const moment = (symbol, twoPoints, am) => {
+//   let date = new Date();
+//   let months = ["Yanvar", "Fevral", "Mart"];
+
+//   let format = [
+//     `${
+//       months[date.getMonth()]
+//     } ${date.getDate()}${symbol} ${date.getFullYear()} ${date.getHours()}${twoPoints}${date.getMinutes()} ${am}`,
+//   ];
+//   let LLL = format;
+//   return `presentDay: ${LLL}`;
+// };
+// console.log(moment(",", ":", "AM")); // presentDay: Mart 1, 2023 10:10
+
+// ============== lll =========
+
+// const moment = (symbol, twoPoints, am) => {
+//   let date = new Date();
+//   let months = ["Yanvar", "Fevral", "Mart"];
+//   let format = [
+//     `${months[date.getMonth()].slice(
+//       0,
+//       3
+//     )} ${date.getDate()}${symbol} ${date.getFullYear()} ${date.getHours()}${twoPoints}${date.getMinutes()} ${am}`,
+//   ];
+//   let lll = format;
+//   return `presentDay: ${lll}`;
+// };
+
+// console.log(moment(",", ":", "AM")); // presentDay: Mar 1, 2023 10:23 AM
+
+//========== LLLL ======
+
+// const moment = (symbol, twoPoints, am) => {
+//   let date = new Date();
+//   let months = ["Yanvar", "Fevral", "Mart"];
+//   let week = [
+//     "Dushanba",
+//     "Seshanba",
+//     "Chorshanba",
+//     "Payshanba",
+//     "Juma",
+//     "Shanba",
+//     "Yakshanba",
+//   ];
+//   let format = [
+//     `${week[date.getDay() - 1]}${symbol} ${
+//      months weeks[date.getMonth()]
+//     } ${date.getDate()}${symbol} ${date.getFullYear()} ${date.getHours()}${twoPoints}${date.getMinutes()} ${am}`,
+//   ];
+//   let llll = format;
+//   return `presentDay: ${llll}`;
+// };
+
+// console.log(moment(",", ":", "AM")); // presentDay: Chorshanba, Mart 1, 2023 10:56 AM
+
+// ================== llll ==========
+
+// const moment = (symbol, twoPoints, am) => {
+//   let date = new Date();
+//   let months = ["Yanvar", "Fevral", "Mart"];
+//   let week = [
+//     "Dushanba",
+//     "Seshanba",
+//     "Chorshanba",
+//     "Payshanba",
+//     "Juma",
+//     "Shanba",
+//     "Yakshanba",
+//   ];
+//   let format = [
+//     `${week[date.getDay() - 1].slice(0, 4)}${symbol} ${months[
+//       date.getMonth()
+//     ].slice(
+//       0,
+//       3
+//     )} ${date.getDate()}${symbol} ${date.getFullYear()} ${date.getHours()}${twoPoints}${date.getMinutes()} ${am}`,
+//   ];
+//   let llll = format;
+//   return `presentDay: ${llll}`;
+// };
+
+// console.log(moment(",", ":", "AM")); // presentDay: Chor, Mar 1, 2023 10:55 AM
+
+// const moment = (symbol) => {
+//   let months = ["yanvar", "fevral", "mart"];
+
+//   let date = new Date();
+
+//   return `tody: ${
+//     months[date.getMonth()]
+//   } ${date.getDate()} ${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${symbol}`;
+// };
+// console.log(moment("am")); // tody: mart 1 2023, 11:5:58 am
+
+// const moment = () => {
+//   let week = [
+//     "Dushanba",
+//     "Seshanba",
+//     "Chorshanba",
+//     "Payshanba",
+//     "Juma",
+//     "Shanba",
+//     "Yakshanba",
+//   ];
+
+//   let format = [`${week[date.getDay() - 1]}`];
+//   let dddd = format;
+//   return `week: ${dddd}`;
+// };
+
+// console.log(moment());
+
+// const moment = () => {
+//   let format = `${months[date.getMonth()].slice(0, 3)} ${date.getDate()} ${date
+//     .getFullYear()
+//     .toString()
+//     .slice(2, 4)}`;
+//   let MMM = format;
+//   return `MMM Do YY: ${MMM}`;
+// };
+// console.log(moment()); // MMM Do YY: mar 1 23
