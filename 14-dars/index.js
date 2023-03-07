86674b9862cf96691cf545dc5e06323ec7d811db
Samar0208 <ttymi1994@gmail.com>
2023-03-07 17:45:29 +0500

    scope

---
+++ 14-dars/index.js
@@ -0,0 +1,43 @@
+// let name = "webbrain";
+
+// function getName() {
+//   console.log(name);
+//   let title = "IT";
+//   function test() {
+//     console.log();
+//   }
+// }
+// getName();
+
+// for (var i = 1; i < 10; i++) {}
+// console.log(i);
+
+// let i = "toshmat";
+// let i = "eshmat";
+// console.log(i);
+
+//IEF
+
+// (function () {
+//   var name = "toshmat";
+//   console.log(name);
+// })();
+
+// function moment() {
+//   moment.locale = () => {
+//     console.log(Date.now());
+//   };
+// }
+
+// moment();
+// moment.locale();
+
+// let obj = {};
+// function set() {
+//   obj.name = "Samar";
+// }
+// set();
+// console.log(obj.name);
+
+console.log();
+setTimeout(() => {}, timeout);
