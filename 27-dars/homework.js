6a97501ef6bad754b49183e0b9a45d575163237f
Samar0208 <ttymi1994@gmail.com>
2023-03-29 15:21:50 +0500

    DOM2

---
+++ 27-dars/homework.js
@@ -0,0 +1,2 @@
+//  DOM - bu document objekt model bunga html dagi barcha malumotlar kiradi.
+// parent, child, siblen.
---
+++ 27-dars/savollar.js
@@ -0,0 +1,12 @@
+// DOM
+
+// console.log(div.hasAttribute("name"))
+// hasAttribute - bu div ning ichida attribute bor yo'qligini aniqlaydi;
+
+// div.setAttribute("id", "btn");
+// setAttribute - bu ikkita parametir qabul qladi attribut va id nomi yoziladi;
+
+// console.log(div.getAttribute("id"));
+// gitAttribute - bu qushilgan atributning nomoni chiqarib beradi;
+
+// removeAttribute - bu attributlarni uchirivoradi
